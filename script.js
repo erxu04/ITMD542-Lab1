0efe73a9a01f78813c53caa960de771fc361907f
Emily Xu <emojibear123@gmail.com>
2026-01-27 18:07:28 -0600

    implement collapsible mobile menu

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function expandCollapse() {
   let navList = document.getElementById("nav-list");
   navList.classList.toggle("hidden");
+
+  console.log("clicked");
+  console.log(navList.classList);
 }
